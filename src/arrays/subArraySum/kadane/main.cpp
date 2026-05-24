06494cdf571cc482a4e737842cefa42e1da6e47b
hzd4m <seu@email.com>
2026-05-23 23:30:09 -0300

    feat: Algoritmo de Kadane

---
+++ src/arrays/subArraySum/kadane/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std; 
+
+int kadane(vector<int>& arr, int n){
+    int maior = 0;
+    int soma_atual = 0;
+    for(int i = 0; i < n; i++){
+        soma_atual = soma_atual + arr[i];
+        maior = max(soma_atual, maior); 
+        if(soma_atual < 0){ 
+            soma_atual = 0;
+            
+        }
+       
+        
+        
+        
+    }
+    
+    return maior;
+} 
+
+int main(){
+    vector<int> array = {-2,3,4,-1,5,-12,6,1,3}; 
+    int n = array.size(); 
+    
+    cout << kadane(array,n) << endl; 
+    
+    return 0;
+}
